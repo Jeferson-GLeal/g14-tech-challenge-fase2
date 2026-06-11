b0be7eb25b57cbe3df5c4047bc606929ee3f3b8e
Fabio Machado <fabio.a.machado@hotmail.com>
2026-06-10 21:52:06 -0300

    feat: criação da estrutura primária da arquitetura do projeto

---
+++ foodlink-api/src/main/java/com/fiap/foodlink_api/FoodlinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.foodlink_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodlinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodlinkApiApplication.class, args);
+	}
+
+}
